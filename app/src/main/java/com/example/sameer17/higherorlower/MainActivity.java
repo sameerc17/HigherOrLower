382febeaec2b8d6d4ac7e3fdf87ce86382663910
Sameer <sameer1771999@gmail.com>
2018-11-22 20:47:29 +0530

    Added try catch

--- app/src/main/java/com/example/sameer17/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/sameer17/higherorlower/MainActivity.java
@@ -5,39 +5,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
-    int randno;
+    int randno,guessno;
     public void guess(View view)
     {
         EditText guessEditText =(EditText) findViewById(R.id.guessEditText);
-        int guessno=Integer.parseInt(guessEditText.getText().toString());
-        if(guessno>randno)
+        try{
+            guessno=Integer.parseInt(guessEditText.getText().toString());
+        }catch (Exception e)
         {
-            Toast.makeText(MainActivity.this,"Your guess is higher!!!! Try again!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this,"Please enter a valid value!!",Toast.LENGTH_LONG).show();
         }
-        else
-        if(guessno<randno)
-        {
-            Toast.makeText(MainActivity.this,"Your guess is lower! Try again",Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            Toast.makeText(MainActivity.this,"Your guess is correct! Congratulations! Let's try another number now.",Toast.LENGTH_LONG).show();
-            Random rand=new Random();
-            randno=rand.nextInt(50)+1;
-        }
+            if (guessno > 0 && guessno<51) {
+                if (guessno > randno)
+                {
+                    Toast.makeText(MainActivity.this, "Your guess is higher !! Try again!", Toast.LENGTH_SHORT).show();
+                }
+                if (guessno < randno)
+                {
+                    Toast.makeText(MainActivity.this, "Your guess is lower !! Try again!", Toast.LENGTH_SHORT).show();
+                }
+                if (guessno == randno)
+                {
+                    Toast.makeText(MainActivity.this, "Your guess is correct! Congratulations! Let's try another number now.", Toast.LENGTH_LONG).show();
+                    Random rand = new Random();
+                    randno = rand.nextInt(50) + 1;
+                }
+            }
+            else
+            {
+                Toast.makeText(MainActivity.this,"Please enter a valid value!!",Toast.LENGTH_LONG).show();
+            }
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Random rand=new Random();
         randno=rand.nextInt(50)+1;
-
     }
 }
